61a3640b0fbd6ff51cb575bd51c825f5aeb2fa42
nathanjh-28 <nathan@nathanjharris.com>
2020-10-07 17:22:25 -0600

    checkout session working

--- payments/static/main.js
+++ payments/static/main.js
@@ -1,7 +1,25 @@
-console.log('sanity check')
+console.log("Sanity check!");
 
-fetch('/config/')
-.then((result) => {return result.json(); })
+// Get Stripe publishable key
+fetch("/config/")
+.then((result) => { return result.json(); })
 .then((data) => {
-    const stripe = Stripe(data.publicKey);
-})
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+
+  // new
+  // Event handler
+  document.querySelector("#submitBtn").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
