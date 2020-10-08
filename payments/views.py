44e62379e7f0395af775b1e54a8ea454cc2b9e79
nathanjh-28 <nathan@nathanjharris.com>
2020-10-07 22:03:45 -0600

    stripe webhooks working

--- payments/views.py
+++ payments/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 from django.conf import settings
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 import stripe
@@ -55,3 +55,25 @@ class SuccessView(TemplateView):
 
 class CancelledView(TemplateView):
     template_name = 'cancelled.html'
+
+@csrf_exempt
+def stripe_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try: 
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+    if event['type'] == 'checkout.session.completed':
+        print('Payment was successful.')
+        #run some custom code here
+    
+    return HttpResponse(status=200)
